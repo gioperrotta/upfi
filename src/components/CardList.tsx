997c0fb956904fdd8974e9519c3a07305b69b9e3
Giovanni Perrotta <gioperrotta@gmail.com>
2021-12-14 23:25:22 -0300

    Ignite-REACTJS challange-upfi- final

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -17,16 +17,31 @@ interface CardsProps {
 
 export function CardList({ cards }: CardsProps): JSX.Element {
   // TODO MODAL USEDISCLOSURE
-
+  const { onOpen, isOpen, onClose } = useDisclosure();
   // TODO SELECTED IMAGE URL STATE
-
+  const [imgUrl, setImgUrl] = useState('');
   // TODO FUNCTION HANDLE VIEW IMAGE
+  function handleViewImage(url: string) {
+    onOpen();
+    setImgUrl(url);
+  }
 
   return (
     <>
       {/* TODO CARD GRID */}
+      <SimpleGrid columns={[1, 2, 3]} spacing="40px">
+        {cards.map(card => (
+          <Card key={card.id} data={card} viewImage={handleViewImage}/>
+        ))}
+
+      </SimpleGrid>
 
       {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage
+          isOpen={isOpen}
+          imgUrl={imgUrl}
+          onClose={onClose}
+      />
     </>
   );
 }
